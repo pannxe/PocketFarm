3872a1bdf1d56595473d40bd9603df45bc524d62
Krit Patrix <kzt.patrix@gmail.com>
2021-11-17 14:48:44 +0700

    refactor code base

--- server_comm.py
+++ server_comm.py
@@ -4,40 +4,21 @@ import paho.mqtt.client as mqtt
 import json
 
 
-def on_message(client, obj, msg):
-    print("Got a payload: ", end="")
-    cmd = msg.payload.decode("utf-8")
-    print(cmd)
-    cmd = cmd.lower()
-    if cmd == "stat":
-        send_stat()
-    elif cmd == "edit":
-        print("edit\n  Applying new configuration")
-        m_in = json.loads(cmd)
-        f = open("now_setting.conf", "w")
-        f.write("{0} {1} {2} {3}".format(
-            m_in["temp"], m_in["humi"], m_in["mois"], m_in["lumi"]
-            )
-        )
-        f.close()
-        print("Done\n")
+def sendStat(brokerOut):
+    dataOut = json.dumps(brokerOut)
+    mqttc.publish("/test2", dataOut)
+    print("Finished\nWaiting for the next payload...")
 
 
-def send_stat():
-    print("Waiting for answer... ")
-    buffer = get_answer()
-    print("Finished getting answer, sending data to Line... ",)
-    broker_out = {
-        "in_humi": buffer[2], "in_temp": buffer[3], "out_humi": buffer[4],
-        "out_temp": buffer[5], "mois": buffer[6], "lumi": buffer[7]
-    }
-    print(str(buffer))
-    data_out = json.dumps(broker_out)
-    mqttc.publish("/test2", data_out)
-    print("Finished, Waiting for the next payload\n")
+def applyStat(cmd) :
+    print("Applying new configuration...", end="")
+    m_in = json.loads(cmd)
+    with open("now_setting.conf", "w") as f :
+        f.write(f"{m_in['temp']} {m_in['humi']} {m_in['mois']} {m_in['lumi']}")
+    print("Done\n")
 
 
-def get_answer():
+def getAnswer():
     r_flag = Flag(open("request.flg", "r+"))
     a_flag = Flag(open("answer.flg", "r+"))
     b_flag = Flag(open("busy.flg", "r+"))
@@ -52,13 +33,14 @@ def get_answer():
     print("Set r_flag to REQUESTED... ", end="")
     r_flag.set_stat([cf.Comp.LINE, cf.Stat.REQUESTED, 0, 0, 0, 0, 0, 0])
     print("Done")
+
     while True:
         print("  Getting a_flag data... ")
         a_flag.get_data()
-        print("    got --> " + str(a_flag.buffer))
+        print(f"    got --> {a_flag.buffer}")
         print("  Getting r_flag data... ")
         r_flag.get_data()
-        print("    got --> " + str(r_flag.buffer))
+        print(f"    got --> {r_flag.buffer}")
         if a_flag.is_answered() and r_flag.buffer:
             print("  Set r_flag to ACQUIRED... ", end="")
             r_flag.set_stat([cf.Comp.LINE, cf.Stat.ACQUIRED, 0, 0, 0, 0, 0, 0])
@@ -70,12 +52,28 @@ def get_answer():
             del b_flag
             return buffer
 
+def onMessage(client, obj, msg):
+    cmd = msg.payload.decode("utf-8").lower()
+    print(f"Got a payload: {cmd}")
+
+    if cmd == "stat":
+        print("Waiting for answer... ")
+        buffer = getAnswer()
+        print(f"Finished getting answer.\nBuffer : {buffer}")
+
+        brokerOut = {
+        "in_humi": buffer[2], "in_temp": buffer[3], "out_humi": buffer[4],
+        "out_temp": buffer[5], "mois": buffer[6], "lumi": buffer[7]
+        }
+        sendStat(brokerOut)
+    elif cmd == "edit":
+        applyStat(cmd)
 
 print("Pocket Farm's")
 print("LINE COMMUNICATION MODULE\nVERSION 1.0.0\n_________________________\n")
 print("Initialising... ", end="")
 mqttc = mqtt.Client()
-mqttc.on_message = on_message
+mqttc.on_message = onMessage
 mqttc.username_pw_set("brsiutlc", "Rw4rcSFm_gCL")
 mqttc.connect('m15.cloudmqtt.com', 17711)
 mqttc.subscribe("/test1", 0)
